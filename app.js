7208fb2c0e262667f987a6433499f26fc427cd62
yuliia-askarova <lastsong428@gmail.com>
2024-03-19 16:38:44 +0000

    some changes were made

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const port = 3000
+const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, './client/dist')));
 
 app.get('*', (req, res) => {
